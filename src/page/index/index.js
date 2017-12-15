ed89f765a3365397f7a9f14f16d31ccf805df13e
kally <188963758@qq.com>
2017-12-15 17:42:42 +0800

    更新目录

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,24 +2,44 @@
  * @Author: Administrator
  * @Date:   2017-12-12 09:06:55
  * @Last Modified by:   Administrator
- * @Last Modified time: 2017-12-12 10:26:59
+ * @Last Modified time: 2017-12-15 14:20:02
  */
 
 require('./index.css');
-require('node_modules/pagination/release/pagination.full.min.js');
-var pagination = require('pagination');
 
-// console.log(pagination);
 
-// (function() {
-//     var paginator = new pagination.ItemPaginator({ prelink: '/', current: 3, rowsPerPage: 200, totalResult: 10020 });
-//     var html = paginator.render();
-//     var paginator = pagination.create('search', { prelink: '/', current: 1, rowsPerPage: 200, totalResult: 10020 });
-//     html += paginator.render();
-//     var ele = document.getElementById("paging");
-//     console.log(ele);
-//     ele.innerHTML = html;
-// })();
+var pagination = require('pagination')
 
-var paginator = pagination.create('search', {prelink:'/', current: 1, rowsPerPage: 200, totalResult: 10020});
-console.log(paginator.render());
+var boostrapPaginator = new pagination.TemplatePaginator({
+	prelink:'/', current: 3, rowsPerPage: 200,
+	totalResult: 10020, slashSeparator: true,
+	template: function(result) {
+		var i, len, prelink;
+		var html = '<div><ul class="pagination">';
+		if(result.pageCount < 2) {
+			html += '</ul></div>';
+			return html;
+		}
+		prelink = this.preparePreLink(result.prelink);
+		if(result.previous) {
+			html += '<li><a href="' + prelink + result.previous + '">' + this.options.translator('PREVIOUS') + '</a></li>';
+		}
+		if(result.range.length) {
+			for( i = 0, len = result.range.length; i < len; i++) {
+				if(result.range[i] === result.current) {
+					html += '<li class="active"><a href="' + prelink + result.range[i] + '">' + result.range[i] + '</a></li>';
+				} else {
+					html += '<li><a href="' + prelink + result.range[i] + '">' + result.range[i] + '</a></li>';
+				}
+			}
+		}
+		if(result.next) {
+			html += '<li><a href="' + prelink + result.next + '" class="paginator-next">' + this.options.translator('NEXT') + '</a></li>';
+		}
+		html += '</ul></div>';
+		return html;
+	}
+});
+console.log(boostrapPaginator.render());
+
+$('#data-container').html(boostrapPaginator.render());
