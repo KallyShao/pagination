04eb83a3ec9683ca66f785eac29b6549db4d862b
KallyShao <188963758@qq.com>
2017-12-17 21:48:23 +0800

    cf分页测试成功

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,7 @@
  * @Author: Administrator
  * @Date:   2017-12-12 09:06:55
  * @Last Modified by:   Administrator
- * @Last Modified time: 2017-12-17 14:35:12
+ * @Last Modified time: 2017-12-17 21:44:55
  */
 
 require('./index.css');
@@ -17,6 +17,8 @@ var templateIndex   = require('./index.string');
 
 var page = {
 	data:{
+		productList: null,
+		pageSize: 3,
 		listParam : {
 		    keyword         : 'i',
 		    // keyword         : _cf.getUrlParam('keyword')    || '',
@@ -24,7 +26,15 @@ var page = {
 		    // orderBy         : _cf.getUrlParam('orderBy')    || 'default',
 		    // pageNum         : _cf.getUrlParam('pageNum')    || 1,
 		    // pageSize        : _cf.getUrlParam('pageSize')   || 20
-		    pageSize        : _cf.getUrlParam('pageSize')   || 2
+		    // pageSize        : _cf.getUrlParam('pageSize')   || 2
+		},
+		pageParam: {
+			hasPreviousPage : false,	//是否有前一页
+			prePage         : 0,		//前一页是几(num)
+			hasNextPage     : false,
+			nextPage        : 0,
+			pageNum         : 1,	//当前页
+			pages           : 10	//总页数
 		}
 	},
 	init: function(){
@@ -37,26 +47,36 @@ var page = {
 	loadList: function(){
 		var _this       = this,
 		    listHtml    = '',
-		    listParam   = this.data.listParam;
-		    $pListCon   = $('.p-list-con');
+		    listParam   = this.data.listParam,
+		    pageParam   = this.data.pageParam;
+		$pListCon   = $('.p-list-con');
 		$pListCon.html('<div class="loading"></div>');
 
 		_product.getProductList(listParam, function(res){
-			console.log(res.list);
+			//console.log(res.list);
+			_this.data.productList = res.list;
+			// console.log(_this.data.productList);
+			var listLen = res.list.length;
+			_this.data.pageParam.pages = Math.ceil(listLen / _this.data.pageSize);
+			console.log(_this.data.pageParam.pages);
+			if(_this.data.pageParam.pages > 1){
+				_this.data.pageParam.hasNextPage = true;
+			}
+
+			var resList;
+			if(_this.data.pageParam.pages > 1){
+            	//前pageSize个
+            	resList = _this.data.productList.slice(0, _this.data.pageSize);
+            }else{
+            	resList = res.list;
+            }
 
 			listHtml = _cf.renderHtml(templateIndex, {
-                list :  res.list
+                list :  resList
             });
             $pListCon.html(listHtml);
 
-			_this.loadPagination({
-				hasPreviousPage : res.hasPreviousPage,	//是否有前一页
-				prePage         : res.prePage,	//前一页是几(num)
-				hasNextPage     : res.hasNextPage,
-				nextPage        : res.nextPage,
-				pageNum         : res.pageNum,	//当前页
-				pages           : res.pages
-			})
+			_this.loadPagination(pageParam);	//这里你的pageParam就是形参pageInfo
 
 		}, function(errMsg){
 			_cf.errorTips(errMsg);
@@ -70,8 +90,37 @@ var page = {
 	   this.pagination.render($.extend({}, pageInfo, {
 	   		container: $('.pagination'),
 	   		onSelectPage: function(pageNum){
-	   			_this.data.listParam.pageNum = pageNum;
-	   			_this.loadList();
+	   			//_this.data.pageParam.pageNum = pageNum;
+	   			//_this.loadList();
+	   			_this.data.pageParam.pageNum = pageNum;
+	   			if(_this.data.pageParam.pages > pageNum){
+	   				_this.data.pageParam.hasNextPage = true;
+	   				_this.data.pageParam.nextPage = pageNum + 1;
+	   			} else {
+	   				_this.data.pageParam.hasNextPage = false;
+	   			}
+
+	   			if(pageNum <= 1){
+	   				_this.data.pageParam.hasPreviousPage = false;
+	   			}else{
+	   				_this.data.pageParam.hasPreviousPage = true;
+	   				_this.data.pageParam.prePage = pageNum - 1;
+	   			}
+	   			
+				var resList;
+				if(_this.data.pageParam.pages > pageNum){
+					// resList = _this.data.productList[pageNum * 10: pageNum * 10 + 10];
+					resList = _this.data.productList.slice((pageNum -1) * _this.data.pageSize, pageNum * _this.data.pageSize);
+				}else{
+					// resList = _this.data.productList[pageNum * 10: ]
+					resList = _this.data.productList.slice((pageNum -1) * _this.data.pageSize);
+				}
+
+				listHtml = _cf.renderHtml(templateIndex, {
+                	list :  resList
+            	});
+            	$pListCon.html(listHtml);
+				_this.loadPagination(_this.data.pageParam);
 	   		}
 	   }));
 	}
